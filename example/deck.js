f9dacf0454251b0ced67374685048ea7389afdc6
liuderchi <liuderchi@gmail.com>
2019-01-15 13:29:27 +0800

    apply webfont

--- example/deck.js
+++ example/deck.js
@@ -5,12 +5,12 @@ import demo from './topics/demo.mdx'
 import qAndA from './topics/q-and-a.mdx'
 import conclusion from './topics/conclusion.mdx'
 
-// import './fonts/Webfonts/futurapt_book_macroman/stylesheet.css'
-// import './fonts/Webfonts/futurapt_bookitalic_macroman/stylesheet.css'
-// import './fonts/Webfonts/futurapt_demi_macroman/stylesheet.css'
-// import './fonts/Webfonts/futurapt_demiitalic_macroman/stylesheet.css'
-// import 'typeface-space-mono'
-// import 'typeface-spectral'
+import './fonts/Webfonts/futurapt_book_macroman/stylesheet.css'
+import './fonts/Webfonts/futurapt_bookitalic_macroman/stylesheet.css'
+import './fonts/Webfonts/futurapt_demi_macroman/stylesheet.css'
+import './fonts/Webfonts/futurapt_demiitalic_macroman/stylesheet.css'
+import 'typeface-space-mono'
+import 'typeface-spectral'
 
 export { default as theme } from 'gatsby-mdx-theme'
 export default [
